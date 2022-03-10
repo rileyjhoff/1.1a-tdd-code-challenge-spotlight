434886f7e37fefafbc3d6695d815e4a9844713bb
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-10 11:19:32 -0800

    test fails for divideThenMultiply

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 import { 
-    addExclamationPoints, multiplyBySeven, multiplyBy12ThenHalve,
+    addExclamationPoints, multiplyBySeven, multiplyBy12ThenHalve, divideThenMultiply,
 } from '../functions.js';
 
 const { test, skip } = QUnit;
@@ -42,6 +42,22 @@ test('this test should pass for multiplyBy12ThenHalve', (expect) => {
     const expected = 24;
 
     const actual = multiplyBy12ThenHalve(4);
+
+    expect.equal(actual, expected);
+});
+
+skip('this test should be skipped', (expect) => {
+    const expected = true;
+
+    const actual = true;
+
+    expect.equal(actual, expected);
+});
+
+test('this test should pass for divideThenMultiply', (expect) => {
+    const expected = 10;
+
+    const actual = divideThenMultiply(8, 4, 5);
 
     expect.equal(actual, expected);
 });
